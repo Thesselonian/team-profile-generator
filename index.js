97e4f7bdc02b0c36fb812e48d8073d525493e7b5
Eric <ericyoung111@gmail.com>
2021-11-14 19:56:43 -0700

    staretd working on the HTML generation and formatting the employees array

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const promptManagerQuestions = require('./lib/managerQuestions')
 const typeOfEmployee = require('./lib/employeeQuestions')
+const generateBodyHTML = require('./lib/HTMLGenerator')
 
 promptManagerQuestions()
 .then(typeOfEmployee)
+.then(generateBodyHTML)
